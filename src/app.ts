6231d694fb6533618a3ef4f5525000daa2941839
Angel Suarez <112484606+AngelBlackBlue@users.noreply.github.com>
2025-05-11 14:01:14 -0300

    update

--- src/app.ts
+++ src/app.ts
@@ -6,20 +6,20 @@ const esPrimo = (n: number): boolean => {
   if (n <= 1) return false;
   if (n === 2) return true;
   if (n % 2 === 0) return false;
-  for (let i = 3; i <= Math.sqrt(n); i += 2) {
+  for (let i = 3; i <= Math.sqrt(n); i += 1) {
     if (n % i === 0) return false;
   }
   return true;
 };
 
-app.get('/api/primo/:numero', (req: Request, res: Response) => {
+app.get('/api/primo/:numero', (req: Request, res: Response):any => {
   const numero = parseInt(req.params.numero);
 
   if (isNaN(numero)) {
     return res.status(400).json({ error: 'El valor no es un número válido' });
   }
 
-  return res.json({ numero, esPrimo: esPrimo(numero) });
+  return res.status(200).json({ numero, esPrimo: esPrimo(numero) });
 });
 
 export default app;
